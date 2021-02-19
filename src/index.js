c4f894963f4c1ab54e6ff7c95e73129c03179352
Duc Huy <phamduchuy372000h@gmail.com>
2021-02-19 19:55:54 +0700

    Deployment

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
+console.log('SUPERMAN', process.env.REACT_APP_NAME);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
--- src/services/httpService.js
+++ src/services/httpService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import logger from './logService';
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 axios.interceptors.response.use(null, error => {
 	const expectedError =
-		error.response &&
-		error.response.status >= 400 &&
-		error.response.status < 500;
+		error.response && error.response.status >= 400 && error.response.status < 500;
 
 	if (!expectedError) {
 		logger.log();
